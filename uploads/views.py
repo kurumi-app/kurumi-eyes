fadd0396ad7766fa49d89cfd8a2d32e5d7dc07d4
clmb <ccolombomatheus@gmail.com>
2022-10-28 18:03:39 +0100

    Feat/Refactor: Dashboard

--- uploads/views.py
+++ uploads/views.py
@@ -1,10 +1,18 @@
-import re
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 # Create your views here.
+
+login_url = 'login'
+
 def home(request):
     return render(request, 'pages/home.html')
 
+@login_required(login_url=login_url)
 def dashboard(request):
-    return render(request, 'pages/dashboard.html')
+    context = {
+        'users': get_user_model().objects.count(),
+    }
+    return render(request, 'pages/dashboard.html', context)
 
