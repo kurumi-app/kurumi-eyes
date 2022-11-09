a82a273d8b826f3318dd115eea24229c84352eee
clmb <ccolombomatheus@gmail.com>
2022-11-09 18:19:23 +0000

    Feat: Settings view and configuration

--- uploads/urls.py
+++ uploads/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from .views import *
 
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -12,5 +12,8 @@ urlpatterns = [
     path("upload/<slug:slug>/", upload_info, name="upload_info"),
     path("download/<slug:slug>/", download_image, name="download"),
     path("myuploads/", my_uploads, name="my_uploads"),
+
+    path("settings/", user_settings, name="settings"),
+    path("sharex/", sharex_conf, name="sharex_conf"),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- uploads/views.py
+++ uploads/views.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse, FileResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.db.models import Sum
+from rest_framework.authtoken.models import Token
 from .forms import *
 import datetime
+import json 
+
 # Create your views here.
 
 login_url = 'login'
@@ -67,3 +70,37 @@ def my_uploads(request):
         'uploads': uploads,
     }
     return render(request, 'pages/myuploads.html', context)
+
+# settings
+
+def user_settings(request):
+    return render(request, 'pages/settings.html')
+
+def sharex_conf(request):
+    user = request.user
+    token = Token.objects.get(user=user)
+
+    data = {
+    "Version": "1.0.0",
+    "Name": "kurumi-eyes",
+    "DestinationType": "ImageUploader, FileUploader",
+    "RequestMethod": "POST",
+    "RequestURL": "https://kurumi-eyes.cyou/api/upload/",
+    "Headers": {
+        "Authorization": f"Token {token}"
+    },
+    "Body": "MultipartFormData",
+    "FileFormName": "image",
+    "URL": "{json:upload_url}",
+    "ErrorMessage": "{json:displayMessage}"
+    }
+
+    with open(f'{user}.sxcu', 'w') as f:
+        json.dump(data, f, indent=2)
+
+    response = FileResponse(open(f'{user}.sxcu', 'rb'))
+    response['Content-Disposition'] = f'attachment; filename={user}.sxcu'
+    return response
+
+
+    
